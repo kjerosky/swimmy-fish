e13f2d83bf7795c601b924173eb8a6d3be64a9bd
Keith Jerosky <kjerosky@gmail.com>
2025-06-17 10:30:24 +0100

    Save and load high score data

--- scripts/main_level.gd
+++ scripts/main_level.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var player := $Player
 
 const MOVING_HORIZONTAL_SCROLL_SPEED := Constants.SCROLL_VELOCITY
+const SAVE_FILE_PATH := "user://scores.data"
 
 enum GameState {
 	WAITING_TO_START,
@@ -15,15 +16,21 @@ enum GameState {
 var state: GameState
 
 var current_score := 0
+var high_score := 0
 
 # ---------------------------------------------------------------------------
 
 func _ready() -> void:
+	load_scores()
 	restart()
 
 # ---------------------------------------------------------------------------
 
 func restart() -> void:
+	if current_score > high_score:
+		high_score = current_score
+		save_scores()
+	
 	state = GameState.WAITING_TO_START
 	current_score = 0
 	
@@ -69,3 +76,37 @@ func stop_game() -> void:
 
 func _on_point_scored() -> void:
 	current_score += 1
+
+# ---------------------------------------------------------------------------
+
+func save_scores() -> void:
+	var save_data := {}
+	save_data.high_score = high_score
+	
+	var save_file := FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	if save_file == null:
+		print("[ERROR] Could not save high score: ", FileAccess.get_open_error())
+		return
+	
+	var save_data_json := JSON.stringify(save_data)
+	save_file.store_string(save_data_json)
+
+# ---------------------------------------------------------------------------
+
+func load_scores() -> void:
+	if not FileAccess.file_exists(SAVE_FILE_PATH):
+		print("[WARN] Scores file not found")
+		return
+	
+	var save_file := FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	var save_file_contents := save_file.get_as_text()
+	
+	var json := JSON.new()
+	var parsed_file_contents := json.parse(save_file_contents)
+	if not parsed_file_contents == OK:
+		print("[ERROR] JSON parse error: ", json.get_error_message(), " on line ", json.get_error_line())
+		return
+	
+	var save_data = json.data
+	if "high_score" in save_data:
+		high_score = save_data.high_score
